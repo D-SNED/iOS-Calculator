51ff2e3801b53c883c2d611519deb71aad6530e4
DSnediker <dsnediker7@gmail.com>
2024-07-17 14:08:17 -0700

    starting JS for calculator

--- calculator.js
+++ calculator.js
@@ -1,1 +1,25 @@
-console.log("hellooo");
+
+function buttonClick(value) {
+    if (isNaN(parseInt(value))) {
+        handleSymbol(value);
+    } else {
+        handleNumber(value);
+    }
+}
+
+function handleNumber(number) {
+}
+
+function handleSymbol(symbol) {
+
+}
+
+function init() {
+    console.log('hi');
+    document.querySelector('.calc-buttons')
+    .addEventListener("click", function(event) {
+        buttonClick(event.target.innerText);
+    });
+}
+
+init();
